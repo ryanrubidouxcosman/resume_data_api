cf039eae82f5269a03178f88a695aaa543fb022a
Ethan Rader <rader.ethan@gmail.com>
2020-05-04 21:15:33 -0500

    add skill index and show methods

--- app/controllers/api/skills_controller.rb
+++ app/controllers/api/skills_controller.rb
@@ -1,2 +1,11 @@
 class Api::SkillsController < ApplicationController
+  def index
+    @skills = Skill.all
+    render "index.json.jb"
+  end
+
+  def show
+    @skill = Skill.find_by(id: params["id"])
+    render "show.json.jb"
+  end
 end

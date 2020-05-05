fdabf9e99b1b22961c08d1bb847852cd94278cd7
Ryan Rubidoux Cosman <ryanrubidouxcosman@gmail.com>
2020-05-04 21:21:05 -0500

    educations

--- app/controllers/api/educations_controller.rb
+++ app/controllers/api/educations_controller.rb
@@ -1,2 +1,10 @@
 class Api::EducationsController < ApplicationController
+  def index
+    @educations = Education.all
+  end
+
+  def show
+    education_id = params["id"]
+    @education = Education.find_by(id: education_id)
+    render "show.json.jb"
 end

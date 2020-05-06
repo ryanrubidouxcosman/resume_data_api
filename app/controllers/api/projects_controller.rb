6dabc332a10b2b03ddfd23b59f6cbc6adcc5fc5f
travellwilliams <travellwilliams11@GMAIL.com>
2020-05-05 19:32:32 -0500

    Projects

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    render "index.json.jb"
+  end
+
+  def show
+    @project = Project.find_by(id: params["id"])
+    render "show.json.jb"
+  end
 end
